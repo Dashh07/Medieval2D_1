d8909f393b6162745d6ebc0e63fe12ea405782f7
Dashh07 <aditya.dash0799@gmail.com>
2024-10-07 16:49:55 +0200

    Added attack animation

--- Medieval2D/Assets/Dash/Scripts for Player/PlayerCombat_1.cs
+++ Medieval2D/Assets/Dash/Scripts for Player/PlayerCombat_1.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class PlayerCombat_1 : MonoBehaviour
 {
     public Animator animator;
-    
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
+        Attack();
+    }
+
+    void Attack()
+    {
+
         if (Input.GetButtonDown("Fire1"))
         {
 
 
-           animator.SetBool("isAttack", true);
+            animator.SetBool("isAttack", true);
         }
 
-      
-       
+        if (Input.GetButtonUp("Fire1"))
+        {
 
+
+            animator.SetBool("isAttack", false);
+        }
     }
 
     
