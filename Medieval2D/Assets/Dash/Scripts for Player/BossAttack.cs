49c158537e3ec5a8b6b2e17a0f9c671e8c9f8406
Dashh07 <aditya.dash0799@gmail.com>
2024-10-09 15:46:47 +0200

    some more boss update

--- Medieval2D/Assets/Dash/Scripts for Player/BossAttack.cs
+++ Medieval2D/Assets/Dash/Scripts for Player/BossAttack.cs
@@ -10,16 +10,19 @@ public class BossAttack : MonoBehaviour
     public float attackRange = 1f;
     public LayerMask player;
 
+ 
+
     public void Attack()
     {
-        Vector3 pos = transform.position;
+        
+            Vector3 pos = transform.position;
         pos += transform.right * attackPoint.x;
         pos += transform.up * attackPoint.y;
         Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, player);
 
         if(colInfo != null)
         {
-
+            if (!colInfo.gameObject.CompareTag("Player")) return;
             colInfo.GetComponent<Health>().TakeDamage(attackDamage);
 
         }
